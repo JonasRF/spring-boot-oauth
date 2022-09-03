4499140c5ec15043f7af342b4be5a1dd2cda82d1
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2022-09-03 18:58:12 -0300

    Update spring security oauth java 17

---
+++ src/main/java/com/teste/teste/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.teste.teste.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.teste.teste.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+}
